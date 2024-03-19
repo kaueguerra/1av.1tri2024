3d4124c682608d8d80338b1bd6206686d0868966
kaueguerra <126159335+kaueguerra@users.noreply.github.com>
2024-03-19 07:59:10 -0300

    oi

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const botoes  = document.querySelectorAll(".botoes");
 
-for (let i = 0 < botoes.length; j++;){
+for (let i = i < botoes.length; j++;){
     botoes[i].onclick = function(){
-        for(let j = 0; j<botoes.length; j++){
+        for(let j = 0; j < botoes.length; j++){
             botoes[j].classList.remove("ativo");
         }
     botoes[i].classList.add("ativo");
