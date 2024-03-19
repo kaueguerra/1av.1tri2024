a16b2c1640c302c4938bb50868a174bf2fb327ee
kaueguerra <126159335+kaueguerra@users.noreply.github.com>
2024-03-19 08:09:08 -0300

    jhb

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const botoes  = document.querySelectorAll(".botoes");
 
-for (let i = i < botoes.length; j++;){
+for (let i = i < botoes.length; i++;){
     botoes[i].onclick = function(){
         for(let j = 0; j < botoes.length; j++){
             botoes[j].classList.remove("ativo");
