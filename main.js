82a35bdc23f05a8af00c62ac5b3318b767fdf577
kaueguerra <126159335+kaueguerra@users.noreply.github.com>
2024-04-16 08:17:28 -0300

    kn

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ for (let i = 0; i<botoes.length; i++) {
   }
 }
 const contadores = document.querySelectorAll("contador");
-const tempoObjetivo1 = new Date("2024-12-28T");
+const tempoObjetivo1 = new Date("2024-12-28T00:00:00");
 let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
