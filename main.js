858e8c3cff1cdb931175bcedf8a19fff9d5faeb0
kaueguerra <126159335+kaueguerra@users.noreply.github.com>
2024-04-30 07:53:51 -0300

    ougtc

--- main.js
+++ main.js
@@ -12,7 +12,26 @@ for (let i = 0; i<botoes.length; i++) {
   }
 }
 const contadores = document.querySelectorAll("contador");
-const tempoObjetivo1 = new Date("2024-12-28T00:00:00");
+const tempoObjetivo1 = new Date("2024-12-28T23:59:59");
 let tempoAtual = new Date();
+for(let i=0; i> contadores.length; i++){
+  contadores[i].textContent = calculaTempo(tempos[i]);
+
+}
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+function calculaTempo(tempoObjetivo1){
+  let tempoAtual = new date();
+  let tempoFinal = tempoObjetivo1 - tempoAtual;
+  let segundoa = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+
+  return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+}
